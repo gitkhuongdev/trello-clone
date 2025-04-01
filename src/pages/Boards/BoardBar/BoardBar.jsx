7d1feaeacffa406a71db4c253ad63218b8c7197e
phamhocongkhuong <khuongphc.flamedia@gmail.com>
2025-04-01 21:50:59 +0700

    Mock sort data

---
+++ src/pages/Boards/BoardBar/BoardBar.jsx
@@ -0,0 +1,130 @@
+import Box from '@mui/material/Box';
+import Chip from '@mui/material/Chip';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import VpnLockIcon from '@mui/icons-material/VpnLock';
+import AddToDriveIcon from '@mui/icons-material/AddToDrive';
+import BoltIcon from '@mui/icons-material/Bolt';
+import FilterListIcon from '@mui/icons-material/FilterList';
+import Avatar from '@mui/material/Avatar';
+import AvatarGroup from '@mui/material/AvatarGroup';
+import Tooltip from '@mui/material/Tooltip';
+import Button from '@mui/material/Button';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import { capitalizeFirstLetter } from '~/utils/formatters';
+
+const MENU_STYLES = {
+  color: 'white',
+  bgcolor: 'transparent',
+  border: 'none',
+  paddingX: '5px',
+  borderRadius: '4px',
+  '.MuiSvgIcon-root': {
+    color: 'white',
+  },
+  '&:hover': {
+    bgcolor: 'primary.50',
+  },
+};
+
+function BoardBar({ board }) {
+  return (
+    <Box
+      sx={{
+        width: '100%',
+        height: (theme) => theme.trello.appBarHeight,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        gap: 2,
+        paddingX: 2,
+        overflowX: 'auto',
+        bgcolor: (theme) =>
+          theme.palette.mode === 'dark' ? '#34495e' : '#1976d2',
+        borderBottom: '1px solid white',
+      }}
+    >
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <Chip
+          sx={MENU_STYLES}
+          icon={<DashboardIcon />}
+          label={board?.title}
+          clickable
+          onClick={() => {}}
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<VpnLockIcon />}
+          label={capitalizeFirstLetter(board?.type)}
+          clickable
+          onClick={() => {}}
+        />
+
+        <Chip
+          sx={MENU_STYLES}
+          icon={<AddToDriveIcon />}
+          label='Add to Google drive'
+          clickable
+          onClick={() => {}}
+        />
+
+        <Chip
+          sx={MENU_STYLES}
+          icon={<BoltIcon />}
+          label='Automation'
+          clickable
+          onClick={() => {}}
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<FilterListIcon />}
+          label='Filters'
+          clickable
+          onClick={() => {}}
+        />
+      </Box>
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <Button
+          sx={{
+            color: 'white',
+            borderColor: 'white',
+            '&:hover': { borderColor: 'white' },
+          }}
+          variant='outlined'
+          startIcon={<PersonAddIcon />}
+        >
+          Invite
+        </Button>
+        <AvatarGroup
+          sx={{
+            gap: '10px',
+            '& .MuiAvatar-root': {
+              width: 34,
+              height: 34,
+              fontSize: 16,
+              border: 'none',
+              color: 'white',
+              cursor: 'pointer',
+              '&:first-of-type': { bgcolor: '#a4b0be' },
+            },
+          }}
+          max={3}
+        >
+          <Tooltip title='name'>
+            <Avatar alt='Remy Sharp' src='/static/images/avatar/1.jpg' />
+          </Tooltip>
+          <Tooltip title='name'>
+            <Avatar alt='Sharp' src='/static/images/avatar/1.jpg' />
+          </Tooltip>
+          <Tooltip title='name'>
+            <Avatar alt='harp' src='/static/images/avatar/1.jpg' />
+          </Tooltip>
+          <Tooltip title='name'>
+            <Avatar alt='rp' src='/static/images/avatar/1.jpg' />
+          </Tooltip>
+        </AvatarGroup>
+      </Box>
+    </Box>
+  );
+}
+
+export default BoardBar;
