e7499f13a66bcaff9ee440af8903d470fb0f2c88
phamhocongkhuong <khuongphc.flamedia@gmail.com>
2025-04-09 00:18:44 +0700

    Call API add column, card and fix UI

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -3,8 +3,15 @@ import AppBar from '~/components/AppBar/AppBar';
 import BoardBar from './BoardBar/BoardBar';
 import BoardContent from './BoardContent/BoardContent';
 import { useEffect, useState } from 'react';
-import { fetchBoardDetailsAPI } from '~/apis';
-import { mockData } from '~/apis/mock-data';
+import {
+  fetchBoardDetailsAPI,
+  createNewColumnAPI,
+  createNewCardAPI,
+} from '~/apis';
+
+import { generatePlaceholderCard } from '~/utils/formatters';
+import { isEmpty } from 'lodash';
+// import { mockData } from '~/apis/mock-data';
 
 function Board() {
   const [board, setBoard] = useState(null);
@@ -14,15 +21,63 @@ function Board() {
 
     // Call API
     fetchBoardDetailsAPI(boardId).then((board) => {
+      // Xu ly van de keo tha khi column rong
+      board.columns.forEach((column) => {
+        if (isEmpty(column.cards)) {
+          column.cards = [generatePlaceholderCard(column)];
+          column.cardOrderIds = [generatePlaceholderCard(column)._id];
+        }
+      });
       setBoard(board);
     });
   }, []);
 
+  // Function goi API tao moi column va lam moi du lieu trong state
+  const createNewColumn = async (newColumnData) => {
+    const createdColumn = await createNewColumnAPI({
+      ...newColumnData,
+      boardId: board._id,
+    });
+
+    // Xu ly van de khi tao column moi thi khong keo tha duoc
+    createdColumn.cards = [generatePlaceholderCard(createdColumn)];
+    createdColumn.cardOrderIds = [generatePlaceholderCard(createdColumn)._id];
+
+    // Cap nhat lai state board
+    const newBoard = { ...board };
+    newBoard.columns.push(createdColumn);
+    newBoard.columnOrderIds.push(createdColumn._id);
+    setBoard(newBoard);
+  };
+
+  const createNewCard = async (newCardData) => {
+    const createdCard = await createNewCardAPI({
+      ...newCardData,
+      boardId: board._id,
+    });
+
+    // Cap nhat lai state board
+    const newBoard = { ...board };
+    const columnToUpdate = newBoard.columns.find(
+      (column) => column._id === createdCard.columnId
+    );
+    if (columnToUpdate) {
+      columnToUpdate.cards.push(createdCard);
+      columnToUpdate.cardOrderIds.push(createdCard._id);
+    }
+
+    setBoard(newBoard);
+  };
+
   return (
     <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
       <AppBar />
-      <BoardBar board={mockData.board} />
-      <BoardContent board={mockData.board} />
+      <BoardBar board={board} />
+      <BoardContent
+        board={board}
+        createNewColumn={createNewColumn}
+        createNewCard={createNewCard}
+      />
     </Container>
   );
 }
